723906f2fd99a293f05e9e8a04bcdc2ee1489892
ango <angogcp@gmail.com>
2025-10-22 23:17:35 +0800

    first release 8

--- resources/api-utils.js
+++ resources/api-utils.js
@@ -7,13 +7,10 @@
  */
 function getApiBaseUrl() {
   // Check if we're in a Vercel environmentfunction getApiBaseUrl() {
+    alert("api-utils.js is executing!");
     const isVercel = window.location.hostname.includes('vercel.app') || 
                     window.location.hostname === 'reyfa.vercel.app';
-    console.log('Current hostname:', window.location.hostname);
-    console.log('Is Vercel environment:', isVercel);
-    const baseUrl = isVercel ? '/api' : '';
-    console.log('API Base URL:', baseUrl);
-    return baseUrl;
+    return isVercel ? '/api' : '';
 }
 
 /**
